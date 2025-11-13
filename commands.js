77b7a3e28e89da22b4fb9ab6a0844de142f27901
Moonhell <byteknown@gmail.com>
2025-11-14 00:44:56 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,11 +1,13 @@
+// commands.js
+
 export const ping = {
   data: {
     name: 'ping',
-    description: 'Replies with Pong!'
+    description: 'Replies with Pong!',
   },
   async execute(interaction) {
     await interaction.reply('Pong!');
-  }
+  },
 };
 
 export const say = {
@@ -17,12 +19,12 @@ export const say = {
         name: 'text',
         type: 3, // STRING
         description: 'Text to repeat',
-        required: true
-      }
-    ]
+        required: true,
+      },
+    ],
   },
   async execute(interaction) {
     const text = interaction.options.getString('text');
     await interaction.reply(text);
-  }
+  },
 };
